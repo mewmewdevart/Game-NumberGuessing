8ae47b07e72638576e630f66e530f0c26beb85c7
larcrist@Home <larcrist42@gmail.com>
2022-10-26 23:39:09 -0300

    new banner

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 int random_number();
 
@@ -9,9 +12,18 @@ int main (void)
 	int guess;
 	int life;
 	int randomNumber;
+	int banner;
+	FILE *file;
+	file = fopen("bannerGame.txt", "r");
+	if (file)
+	{
+    		while ((banner = getc(file)) != EOF)//EOF = end of file
+        		printf("%c", banner);
+    		fclose(file);
+	}
 
 	randomNumber = random_number();
-	printf(" Guess a number between 1 and 100!\n");
+	printf("\nGuess a number between 1 and 100!\n");
 	printf("You have a 10 try to guess!\n");
 
 	printf("Number: ");
